8340e275715e77516db55b51ff64db80380d5c34
Dor <dorplaut@gmail.com>
2023-03-05 10:48:35 +0200

    fix: getStaticPaths to dynamic routs

--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -4,8 +4,16 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { usePostsContext } from '@/context/posts';
 import FullPost from '@/components/FullPost';
+// localezation
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Link from 'next/link';
 
 function ShowFullPost() {
+    // localezation
+  const { locale = 'he', locales, push } = useRouter();
+  const { t: translate } = useTranslation('home');
+  // 
   const router = useRouter();
   const postId = router.query.id;
   const [posts, setPosts] = usePostsContext();
@@ -20,19 +28,41 @@ function ShowFullPost() {
       });
     }
   }, [posts]);
-  console.log(post);
 
   return (
     <main className="main">
       <header className="header" id="top">
-        <Header />
+        <Header translate={translate} locales={locales} />
       </header>
       <section>{post && <FullPost flipped post={post} />}</section>
       <footer>
-        <Footer />
+        <Footer translate={translate} />
       </footer>
     </main>
   );
 }
 
 export default ShowFullPost;
+
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'home'])),
+      // Will be passed to the page component as props
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      // String variant:
+      '/articles/64002446583bd642073dce8b',
+      // Object variant:
+      { params: { id: '64002446583bd642073dce8b' } },
+    ],
+    fallback: true,
+  }
+}
+
