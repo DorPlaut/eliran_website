ca6e2366bd382c7f4bb9858b7aa9693660c3ac9a
dorplaut <dorplaut@gmail.com>
2023-03-07 05:57:36 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -21,42 +21,7 @@ export default function Document() {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-                window.interdeal = {
-                  "sitekey": "4e0a51fe24e1e1f92eb2cf2cedc0a7bd",
-                  "Position": "Right",
-                  "Menulang": "HE",
-                  "domains": {
-                    "js": "https://cdn.equalweb.com/",
-                    "acc": "https://access.equalweb.com/"
-                  },
-                  "btnStyle": {
-                    "vPosition": [
-                      "80%",
-                      null
-                    ],
-                    "scale": [
-                      "0.8",
-                      "0.8"
-                    ],
-                    "color": {
-                      "main": "#1876c9"
-                    },
-                    "icon": {
-                      "type": 7,
-                      "shape": "semicircle",
-                      "outline": false
-                    }
-                  }
-                };
-                (function(doc, head, body){
-                  var coreCall = doc.createElement('script');
-                  coreCall.src = interdeal.domains.js + 'core/4.4.1/accessibility.js';
-                  coreCall.defer = true;
-                  coreCall.integrity = 'sha512-tq2wb4PBHqpUqBTfTG32Sl7oexERId9xGHX2O3yF91IYLII2OwM1gJVBXGbEPaLmfSQrIE+uAOzNOuEUZHHM+g==';
-                  coreCall.crossOrigin = 'anonymous';
-                  coreCall.setAttribute('data-cfasync', true );
-                  body? body.appendChild(coreCall) : head.appendChild(coreCall);
-                })(document, document.head, document.body);
+              window.interdeal = { "sitekey": "4e0a51fe24e1e1f92eb2cf2cedc0a7bd", "Position": "Right", "Menulang": "HE", "domains": { "js": "https://cdn.equalweb.com/", "acc": "https://access.equalweb.com/" }, "btnStyle": { "vPosition": [ "80%", null ], "scale": [ "0.8", "0.8" ], "color": { "main": "#00848e", "second": "#ffffff" }, "icon": { "type": 7, "shape": "semicircle", "outline": false } } }; (function(doc, head, body){ var coreCall = doc.createElement('script'); coreCall.src = interdeal.domains.js + 'core/4.4.1/accessibility.js'; coreCall.defer = true; coreCall.integrity = 'sha512-tq2wb4PBHqpUqBTfTG32Sl7oexERId9xGHX2O3yF91IYLII2OwM1gJVBXGbEPaLmfSQrIE+uAOzNOuEUZHHM+g=='; coreCall.crossOrigin = 'anonymous'; coreCall.setAttribute('data-cfasync', true ); body? body.appendChild(coreCall) : head.appendChild(coreCall); })(document, document.head, document.body);
               `,
           }}
         />
